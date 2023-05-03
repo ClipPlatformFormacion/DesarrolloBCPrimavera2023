a80f30413b153f49c34785a74802c1a3643dfaf0
Cristina Nicolàs <cristina.nicolas@clipplatform.com>
2023-05-03 15:13:48 +0200

    Documentación

---
+++ Documentacion/InstalacionDocker.ps1
@@ -0,0 +1,14 @@
+﻿#Install-Module -Name BcContainerHelper -Force
+
+$artifactUrl = Get-BCArtifactUrl -country es -select Latest -type Sandbox -version 20.5
+
+New-BcContainer `
+    -accept_eula `
+    -artifactUrl $artifactUrl `
+    -auth UserPassword `
+    -containerName BC20CU5ES `
+    -includeTestLibrariesOnly `
+    -includeTestToolkit `
+    -updateHosts `
+    -useBestContainerOS `
+    -dns '8.8.8.8'
